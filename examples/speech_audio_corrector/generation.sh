508abc573f52a1964d108419b70f4cf3c23568be
jonojace <jonojace@gmail.com>
2022-02-04 15:24:24 +0000

    added ability to generate for different epochs

--- examples/speech_audio_corrector/generation.sh
+++ examples/speech_audio_corrector/generation.sh
@@ -37,53 +37,96 @@
 #mkdir $OUT_DIR/$VOCODER/test_oovs
 #mv $OUT_DIR/$VOCODER/*.wav $OUT_DIR/$VOCODER/test_oovs
 
+
+##########################################################################################
+# HANDLE IF JOBS WILL BE RUN INTERACTIVELY OR SENT AS SLURM JOBS
+# interactive=true
+# if interactive set sbatch slurm script
+
 ##########################################################################################
 # 24th Jan 2022
 # generate to investigate models ability to do multi-speaker corrections well
 cd ~/fairseq
 
+VOCODER=wav_22050hz_hifigan
+SPLIT=test
+FEATURE_MANIFEST_ROOT=/home/s1785140/data/LJSpeech-1.1/feature_manifest
+TEXT_FILENAME=test_utts_vctk_dev_set_oovs # wordtypes that are in vctk devset but not in LJ Training data
+
 MODELS=(
+#    test_sac_normal_masking2
 #    run2_SAC_baseline_halved_updatefreq
-    run2_SAC_baseline_normal_updatefreq
-    run2_SAC_one_mask_token_per_grapheme_maxtokens25000_halved_updatefreq
-    run2_SAC_random0.5_halved_updatefreq
-    run2_SAC_random0.5_with_ext_speechreps_halved_updatefreq
-    run2_SAC_random_halved_updatefreq
-    run2_SAC_random_with_ext_speechreps_halved_updatefreq
-    run2_SAC_with_ext_speechreps_halved_updatefreq
+#    run2_SAC_baseline_normal_updatefreq
+#    run2_SAC_one_mask_token_per_grapheme_maxtokens25000_halved_updatefreq
+#    run2_SAC_random0.5_halved_updatefreq
+#    run2_SAC_random0.5_with_ext_speechreps_halved_updatefreq
+#    run2_SAC_random_halved_updatefreq
+#    run2_SAC_random_with_ext_speechreps_halved_updatefreq
+#    run2_SAC_with_ext_speechreps_halved_updatefreq
+#    run2_SAC_baseline_lr0.001_halved_updatefreq
+#    run2_SAC_baseline_lr0.00001_halved_updatefreq
+    run2_SAC_replace_middle_eos_with_sos_halved_updatefreq
+    run2_SAC_zero_mask_toks_per_word_halved_updatefreq
+    run2_SAC_dont_remove_dup_codes_maxtoks20000_4gpus_updfreq2
+    run2_SAC_dont_remove_dup_codes_maxtoks20000_halved_updatefreq
 )
-for MODEL in ${MODELS[*]}; do
-    VOCODER=wav_22050hz_hifigan
-    SPLIT=test
-    FEATURE_MANIFEST_ROOT=/home/s1785140/data/LJSpeech-1.1/feature_manifest
-    SAVE_DIR=checkpoints/$MODEL
-    #CHECKPOINT_NUM=650
-    #CHECKPOINT_NAME=epoch${CHECKPOINT_NUM}
-    #CHECKPOINT_PATH=${SAVE_DIR}/checkpoint${CHECKPOINT_NUM}.pt
-    CHECKPOINT_NAME=checkpoint_last
-    CHECKPOINT_PATH=${SAVE_DIR}/${CHECKPOINT_NAME}.pt
-    OUT_DIR=inference/$MODEL/${CHECKPOINT_NAME}
-    TEXT_FILENAME=test_utts_vctk_dev_set_oovs # wordtypes that are in vctk devset but not in LJ Training data
 
-    python -m examples.speech_audio_corrector.generate_waveform_sac ${FEATURE_MANIFEST_ROOT} \
-      --config-yaml config.yaml --gen-subset ${SPLIT} --task speech_audio_corrector \
-      --path ${CHECKPOINT_PATH} --max-tokens 50000 --spec-bwd-max-iter 32 \
-      --results-path ${OUT_DIR} \
-      --vocoder hifigan \
-      --dump-waveforms \
-      --batch-size 32 \
-      --txt-file examples/speech_audio_corrector/${TEXT_FILENAME}.txt \
-      --add-count-to-filename \
-      --use-external-speechreps --use-ext-word2speechreps-p=1.0
+CHECKPOINTS=(
+#    checkpoint3000
+#    checkpoint3500
+#    checkpoint4000
+#    checkpoint4500
+#    checkpoint5000
+    checkpoint_last
+)
 
-    NEW_OUT_DIR=${OUT_DIR}/${VOCODER}/${TEXT_FILENAME}/
-    mkdir -p ${NEW_OUT_DIR}
-    mv ${OUT_DIR}/${VOCODER}/*.wav ${NEW_OUT_DIR}
+for CHECKPOINT_NAME in ${CHECKPOINTS[*]};
+do
 
-#    mkdir: cannot create directory 'inference/run2_SAC_with_ext_speechreps_halved_updatefreq/checkpoint_last/wav_22050hz_hifigan/test_utts_vctk_dev_set_oovs': No such file or directory
-#mv: cannot stat 'inference/run2_SAC_with_ext_speechreps_halved_updatefreq/checkpoint_last/wav_22050hz_hifigan/*.wav': No such file or directory
-#ls inference/run2_SAC_with_ext_speechreps_halved_updatefreq/checkpoint_last/ext_speechreps/wav_22050hz_hifigan/
+    for MODEL in ${MODELS[*]};
+    do
+        ###############################################################################################################
+        # make in paths and out paths
+        SAVE_DIR=checkpoints/$MODEL # where checkpoints for this model are located
+        CHECKPOINT_PATH=${SAVE_DIR}/${CHECKPOINT_NAME}.pt # to load checkpoint
+        OUT_DIR=inference/${MODEL}/${CHECKPOINT_NAME} # to save generated samples
+
+        ###############################################################################################################
+        # LJ test set utts
+        echo "Generating samples for checkpoint ${CHECKPOINT_PATH} using LJ ${SPLIT} set"
+        python -m examples.speech_audio_corrector.generate_waveform_sac ${FEATURE_MANIFEST_ROOT} \
+          --config-yaml config.yaml --gen-subset ${SPLIT} --task speech_audio_corrector \
+          --path ${CHECKPOINT_PATH} --max-tokens 50000 --spec-bwd-max-iter 32 \
+          --results-path ${OUT_DIR} \
+          --vocoder hifigan \
+          --dump-waveforms \
+          --batch-size 32 \
+          --add-count-to-filename \
+          --use-external-speechreps --use-ext-word2speechreps-p 1.0
+
+        # move samples over to folder with more descriptive name
+        NEW_OUT_DIR=${OUT_DIR}/${VOCODER}/LJ_TEST_SET/
+        mkdir -p ${NEW_OUT_DIR}
+        mv ${OUT_DIR}/${VOCODER}/*.wav ${NEW_OUT_DIR}
+
+        ###############################################################################################################
+        # test utts
+        echo "Generating samples for checkpoint ${CHECKPOINT_PATH} using test utts file ${test_utts_vctk_dev_set_oovs}"
+        python -m examples.speech_audio_corrector.generate_waveform_sac ${FEATURE_MANIFEST_ROOT} \
+          --config-yaml config.yaml --gen-subset ${SPLIT} --task speech_audio_corrector \
+          --path ${CHECKPOINT_PATH} --max-tokens 50000 --spec-bwd-max-iter 32 \
+          --results-path ${OUT_DIR} \
+          --vocoder hifigan \
+          --dump-waveforms \
+          --batch-size 32 \
+          --txt-file examples/speech_audio_corrector/${TEXT_FILENAME}.txt \
+          --add-count-to-filename \
+          --use-external-speechreps --use-ext-word2speechreps-p 1.0
+
+        # move samples over to folder with more descriptive name
+        NEW_OUT_DIR=${OUT_DIR}/${VOCODER}/${TEXT_FILENAME}/
+        mkdir -p ${NEW_OUT_DIR}
+        mv ${OUT_DIR}/${VOCODER}/*.wav ${NEW_OUT_DIR}
+    done
 
 done
-
-
